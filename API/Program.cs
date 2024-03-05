e75c3cb533be0d872f784b477935241007266818
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-03-05 19:27:26 +0800

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,23 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+// The two middleware must be invoked in the order specified below.
+app.UseAuthentication(); // asks, do you have a valid token?
+app.UseAuthorization(); // asks, what are you allowed to do?
 
 app.MapControllers();
 
