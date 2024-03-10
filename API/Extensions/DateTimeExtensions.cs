7ae855eb11ba2a951854dc3d0ccf242da5e48547
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-03-10 21:20:06 +0800

    End of section 8

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static int CalculateAge(this DateOnly dob)
+    {
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+        var age = today.Year - dob.Year;
+        
+        // if the bday hasn't occured yet this year.
+        if (dob > today.AddYears(-age)) age--;
+
+        return age;
+    }
+}
