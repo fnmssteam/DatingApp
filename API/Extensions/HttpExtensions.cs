869f160080e86dd9e7c2ce5cefcd598adec03860
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-03-19 21:44:55 +0800

    End of section 13

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using API.Helpers;
+
+namespace API.Extensions;
+
+public static class HttpExtensions
+{
+    public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
+    {
+        var jsonOptions = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+        // Attach custom headers
+        response.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
+
+        // Expose the custom header
+        response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+    }
+}
